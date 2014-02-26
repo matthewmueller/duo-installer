52a804f2ad2a51d7dca9a3a432dcda756d2d369f
Matthew Mueller <mattmuelle@gmail.com>
2014-02-26 01:36:34 +0100

    Initial commit

---
+++ example.js
@@ -0,0 +1,19 @@
+/**
+ * Module Dependencies
+ */
+
+var co = require('co');
+var assert = require('assert');
+var Installer = require('./');
+
+assert(process.env.user, 'no process.env.user');
+assert(process.env.token, 'no process.env.token');
+
+var installer = Installer(__dirname + '/example-app')
+  .auth(process.env.user, process.env.token)
+  .manifest('component.json')
+  .directory('/components')
+
+co(installer.install).call(installer, function(err) {
+  console.log(err);
+});
